dbeba5f526dd6e60d61ce83ff18b932896b75225
Aoldiot <029275@a.cn>
2026-08-16 17:44:51 +0800

    bug

--- backend/start.sh
+++ backend/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 uv run alembic upgrade head
-uv run uvicorn app.main:app --reload --reload-dir app --port 8000
+uv run uvicorn app.main:app --reload --reload-dir app --host 0.0.0.0 --port 8000
